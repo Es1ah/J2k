254c8e67a8b97a349959d1d03ab1005f4c90f333
[dyad] <arnoldandah@gmail.com>
2026-02-05 11:12:59 +0100

    [dyad] Refined React initialization and simplified MapComponent to fix rendering error - wrote 2 file(s)

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -5,8 +5,9 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
-// Fix for default icon issue with Webpack/Leaflet
-delete (L.Icon.Default.prototype as any)._getIconUrl;
+// Fix for default icon issue
+// @ts-ignore
+delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
   iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
@@ -14,21 +15,28 @@ L.Icon.Default.mergeOptions({
 });
 
 interface MapComponentProps {
-  position: [number, number]; // [latitude, longitude]
+  position: [number, number];
   popupText: string;
 }
 
 const MapComponent: React.FC<MapComponentProps> = ({ position, popupText }) => {
   return (
-    <MapContainer center={position} zoom={13} scrollWheelZoom={false} className="h-full w-full rounded-lg">
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={position}>
-        <Popup>{popupText}</Popup>
-      </Marker>
-    </MapContainer>
+    <div className="h-full w-full min-h-[300px]">
+      <MapContainer 
+        center={position} 
+        zoom={13} 
+        scrollWheelZoom={false} 
+        style={{ height: '100%', width: '100%' }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>{popupText}</Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   );
 };
 
